80a9c8f6767797f3a46b9ca0c2af41b71a7614c5
Narukodo <mrnarukodo@gmail.com>
2024-01-26 01:39:50 -0500

    set up typescript webpack tailwindcss boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin")
+
+module.exports = {
+  entry: "./src/index.ts", // entry point of build
+  mode: "development",
+  devServer: {
+    watchFiles: ["src/**/*"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, "src"),
+        use: ["style-loader", "css-loader", "postcss-loader"]
+      }
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [new CopyPlugin({
+    patterns: [{from: "src/index.html", to: "index.html"}]
+  })],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true
+  },
+};
